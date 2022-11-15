0143b8c784f1abb88c0d45863b9eb7ad3e7cb236
Viet Pham <phamviet@users.noreply.github.com>
2022-11-15 16:27:49 +0700

    ff

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -2,17 +2,5 @@
 
 # Usage ./scripts/release.sh
 
-today=$(date +"%F-%s")
-branch="develop"
-
-#git checkout develop
-#
-#git pull --rebase origin develop
-#
-#git checkout -b "$branch"
-#
-#git push -u origin "$branch"
-
-gh pr create --base master --head "$branch" --label release --label feature --title "Release $today" --fill "$@"
-
-#git checkout develop
+title=$(date +"release-%H%M-%F")
+gh pr create --base master --head develop --label release --title "$title" --fill "$@"
