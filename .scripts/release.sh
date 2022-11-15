3dcd68a43e211891b5060635ee39d84bb208d01a
Viet Pham <phamviet@users.noreply.github.com>
2022-11-15 12:55:23 +0700

    no release label

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -4,4 +4,4 @@
 
 today=$(date +"%m-%d-%Y")
 
-gh pr create --base master --head develop --label release --label enhancement --title "Release $today" --fill
+gh pr create --base master --head develop --label enhancement --title "Release $today" --fill
