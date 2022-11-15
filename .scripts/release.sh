16245e8a9501230b0f5ff5826b11b4e8f6089c70
Viet Pham <phamviet@users.noreply.github.com>
2022-11-15 14:52:02 +0700

    squash merge

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -2,15 +2,17 @@
 
 # Usage ./scripts/release.sh
 
-today=$(date +"%F %T")
-branch="release/release-$today"
+today=$(date +"%F-%hh%mm%ss")
+branch="release-$today"
 
 git checkout develop
 
 git pull --rebase origin develop
 
-git checkout -b $branch
+git checkout -b "$branch"
 
-git push -u origin $branch
+git push -u origin "$branch"
 
-gh pr create --base master --head $branch --label release --label enhancement --title "Release $today" --fill
+gh pr create --base master --head "$branch" --label release --label enhancement --title "Release $today" --fill
+
+git checkout develop
